62435e2ee6ecec42c5bbe3b60ec1a555b5f05751
Devesh Prasad <63739986+deveshprasad@users.noreply.github.com>
2021-03-28 23:41:59 +0530

    Updated ggplot.R

--- 1 ggplot.R
+++ 1 ggplot.R
@@ -6,7 +6,6 @@ attach(mydata)
 ggplot(data=mydata[mydata$carat<2.5,],aes(x=carat,y=price,colour=clarity))+
   geom_point(alpha=0.1)+
   geom_smooth()
-
 #ggplot2 package should be installed first and activated
 # if no color  then all will be in black
 #for transparency # to cut area mydata$xaxis<required no
